46e4b8e941db36a7a012d829e9e6a8918520ce31
Wellery Chaves <wellerychaves@outlook.com>
2026-04-15 22:23:56 -0300

    refactor: remove obsolete comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
-// @ts-check
-
-import mdx from "@astrojs/mdx"; // IMPORTANTE
+import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [mdx()],
 	vite: {
