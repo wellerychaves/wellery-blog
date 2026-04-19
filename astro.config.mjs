a62fd31cb7b86777566f3a89bb8e66494ad64ce9
Wellery Chaves <wellerychaves@outlook.com>
2026-04-19 19:30:06 -0300

    chore(config): configure shiki syntax highlighting themes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+	markdown: {
+		shikiConfig: {
+			themes: {
+				light: "gruvbox-light-hard",
+				dark: "tokyo-night",
+			},
+		},
+	},
 	integrations: [mdx()],
 	vite: {
 		plugins: [tailwindcss()],
